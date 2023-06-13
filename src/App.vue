9440c190e579c8c4901ff715559069407cdcfa98
Sumit <sahsumit769@gmail.com>
2023-06-13 10:28:46 +0545

    jenkins check

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Hello this is Sumit Sah. Welcome to  Vue.js App yoo"/>
+    <HelloWorld msg="Hello this is Sumit Sah. Welcome to  Vue.js App yoo testing for jenkins pipeline"/>
   </div>
 </template>
 
